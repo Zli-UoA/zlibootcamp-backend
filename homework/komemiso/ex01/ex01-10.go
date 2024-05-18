968da5ae58f9418ec213e9722f0680d5fdadb35e
komemiso <komemiso0907@gmail.com>
2024-05-18 11:14:33 +0900

    ex01 ex01-10.go

---
+++ homework/komemiso/ex01/ex01-10.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string // 名前
+	age  int    // 年齢
+}
+
+func main() {
+	// Person型のスライスを作成
+	persons := []Person{
+		{"Alice", 20},   // Alice, 20
+		{"Bob", 25},     // Bob, 25
+		{"Charlie", 30}, // Charlie, 30
+	}
+	// スライスの要素を1つずつ取り出して、名前と年齢を出力
+	for _, p := range persons {
+		fmt.Println(p.name, p.age)
+	}
+}
