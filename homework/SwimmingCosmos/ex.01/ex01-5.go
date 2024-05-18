e46b05ec914772e9dbfa4ab5cc7ef107ea27a03f
Konpascreator <76419881+Konpascreator@users.noreply.github.com>
2024-05-18 10:42:26 +0900

    ex01 ex01-5.go

---
+++ homework/SwimmingCosmos/ex.01/ex01-5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	for x := 0; x <= 30; x++ {
+		fmt.Println(x)
+	}
+}
+
+// :=を使って変数を宣言することもできます。var ~でもいける
+// swicth分は　score >= 60 && score <= 50の書き方もいける
