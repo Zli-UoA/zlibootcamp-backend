614537043b549e11e72efd9d8f13eb17ae4b691f
Totto5 <gagagagagagoa@gmail.com>
2024-05-18 10:46:25 +0900

    ex01-(2~9).goの課題提出

---
+++ homework/Totto5/ex01/ex01-3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 2354
+	// ここにコードを書く
+	if x%3 == 0 {
+		fmt.Println("Fizz")
+	} else if x%5 == 0 {
+		fmt.Println("Buzz")
+	} else if x%3 == 0 && x%5 == 0 {
+		fmt.Println("FizzBuzz")
+	} else {
+		fmt.Println(x)
+	}
+}
