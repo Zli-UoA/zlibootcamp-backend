39089325e1c7203e6305f5d6b3384d779feb1f8a
pika0113 <169573978+pika0113@users.noreply.github.com>
2024-05-18 11:31:31 +0900

    ex01 ex01-2.go

---
+++ homework/pika0113/ex01/ex01-2.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 3
+	b := 5
+	fmt.Println(a + b) // 8
+	fmt.Println(a - b) // -2
+	fmt.Println(a * b) // 15
+	fmt.Println(a / b) // 0
+	fmt.Println(a % b) // 3
+	fmt.Println(a * a) // 9
+}
+package main
+import "fmt"
+func main() {
+    x := 2354
+if x % 3 == 0   
+fmt.Println("Fizz")
+if x % 5 == 0
+fmt.Printfln(Buzz) 
+if x % 3 == 0 && x / 5 == 0
+fmt.Println(FizzBuzz)
+if x % 3 == 0 || x % 5 == 0
+fmt.Println(x)}
