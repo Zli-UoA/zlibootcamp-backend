50fed70514b66cd76fbe3e5085ac365406a8c50e
cs3214 <0jj5d2073292w5p@au.com>
2024-05-23 17:05:42 +0900

    ex2-10

--- homework/cs3214/ex01/ex01-7.go
+++ homework/cs3214/ex01/ex01-7.go
@@ -1,19 +1,19 @@
 package main
-import(
-	"fmt"
-	"math"
+import (
+    "fmt"
+    "math"
 )
-	
+
 func main() {
-	x := 2.0
-	z := 1.0
-
-	for i := 0; i <= 10; i++{
-		z -= (z*z - x) / (2*z)
-	}
-
-
+    x := 2.0
+    z := 1.0
+    for d := 1.0; d*d > 1e-10; z -= d { // 条件式を書く
+        d = (z*z - x) / (2*z)
+    }
+    fmt.Println(z)
+    fmt.Println(math.Sqrt(x))
 }
+
 
 
 
