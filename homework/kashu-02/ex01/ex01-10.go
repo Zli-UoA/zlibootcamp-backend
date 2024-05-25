71d8853f1bab864446d2621c985dac334ff5a520
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2024-05-25 11:53:40 +0900

    lec02 answer

---
+++ homework/kashu-02/ex01/ex01-10.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+type Person struct {
+    Name string// 名前
+    Age int// 年齢
+}
+
+func main() {
+    // Person型のスライスを作成
+    persons := []Person{
+        {
+			Name: "Alice",
+			Age: 20,
+		}, // Alice, 20
+		{
+			Name: "Bob",
+			Age: 25,
+		}, // Bob, 25
+		{
+			Name: "Charlie",
+			Age: 30,
+		}, // Charlie, 30
+    }
+    // スライスの要素を1つずつ取り出して、名前と年齢を出力
+    for _, p := range persons {
+        fmt.Println(p.Name, p.Age)
+    }
+}
