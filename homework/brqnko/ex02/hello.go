8cb70326f8b4237cc1388bdd98426a24ddcb254f
brqnko <tenamen.12@gmail.com>
2024-05-25 10:11:34 +0900

    簡単なWebAPIを作成する 演習

--- homework/brqnko/ex02/hello.go
+++ homework/brqnko/ex02/hello.go
@@ -15,6 +15,30 @@ func main() {
 		})
 	})
 
+	r.POST("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, Zli!",
+		})
+	})
+
+	r.DELETE("/bye", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Bye!",
+		})
+	})
+
+	r.Any("/konnichiwa", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Bad request",
+		})
+	})
+
+	r.GET("/kyopro", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "https://atcoder.jp/users/brqnko",
+		})
+	})
+
 	// 演習問題はここに追記してください
 
 	r.Run(":8080") // ポート8080でサーバーを起動
