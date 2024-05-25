9f9b151d15bffa9bdf65b1df256fa43b7c3d95a2
toshi <100521386+ErrwinSmith@users.noreply.github.com>
2024-05-25 11:51:19 +0900

    ex02, api-ex complete

--- homework/Tosheiy/ex-02/hello.go
+++ homework/Tosheiy/ex-02/hello.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -26,10 +26,28 @@ func main() {
 	})
 
 	r.GET("/konnichiwa", func(c *gin.Context) { // HTTP GETで/konnichiwaにアクセスしたときの処理
-		// fmt.Println(gin.Context.Accepted);
 		c.JSON(http.StatusBadRequest, gin.H{ // HTTPステータスコード400(Bad)で、JSONを返す
-			"message": "Bad Request",
+			"error": "Bad Request",
 		})
+	})
+
+	r.GET("/konnichiwa/:code", func(c *gin.Context) { // HTTP GETで/konnichiwaにアクセスしたときの処理
+		code, err := strconv.Atoi(c.Param("code"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"eror": "Bad Request"})
+			return
+		}
+
+		if code == 123 {
+			c.JSON(http.StatusOK, gin.H{"password": 322})
+			return
+		} else if code == 322 {
+			c.JSON(http.StatusOK, gin.H{"status": "clear"})
+			return
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{"error": "code not found."})
 	})
 
 	r.Run(":8080") // ポート8080でサーバーを起動
